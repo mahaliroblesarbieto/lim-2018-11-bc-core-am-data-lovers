aff3fa073f2c27cd2bd4b51adfece72147778dd2
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2018-12-13 11:39:59 -0500

    listadepokemon en data.js / cambio ul en html

--- src/data.js
+++ src/data.js
@@ -1,8 +1,17 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+window.POKEMON  = POKEMON;
+const containerlist = document.getElementById("pokemon-list");
+let list  = '';
+POKEMON.pokemon.forEach((pokemones)  =>  {
+  const li  = `<li>${pokemones.name}</li>`  + `<img src="${pokemones.img}">`;
+  list  = list  + li;
+})
+containerlist.innerHTML = list;
