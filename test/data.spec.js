591335e71557399818122361425834238a8061ca
carocampos <ccampossalazar@gmail.com>
2018-12-19 17:11:07 -0500

    Cambios en test filtrado

--- test/data.spec.js
+++ test/data.spec.js
@@ -2,17 +2,19 @@ require('../src/data.js');
 
 const input = 
 [
-  {id: 1, "num": "001", "name": "Bulbasaur", "type": [
-    "Grass",
-    "Poison"]
+  {id: 1, num: '001', name: 'Bulbasaur', type: [
+    'Grass',
+    'Poison']
   },
-  {"id": 4, "num": "004", "name": "Charmander", "type": [
-      "Fire"]
+  {id: 4, num: '004', name: 'Charmander', type: [
+    'Fire']
   },
-  {"id": 7, "num": "007", "name": "Squirtle", "type": [
-      "Water"]
+  {id: 7, num: '007', name: 'Squirtle', type: [
+    'Water']
   },
 ];
+const output = ['Grass', 'Poison', 'Fire', 'Water'];
+
 
 describe('pokemon', () => {
   it('Debería ser un objeto', () => {
@@ -22,8 +24,8 @@ describe('pokemon', () => {
     it('Debería ser una función', () => {
       expect(typeof window.pokemon.filterData).toBe('function');
     });
-    it('Debería retornar un array de objetos, con la propiedad type' , () => {
-      expect();
+    it('Debería retornar un array de objetos, con la propiedad type', () => {
+      expect(window.pokemon.filterData(input)).toEqual(output);
     });
   });
 });
