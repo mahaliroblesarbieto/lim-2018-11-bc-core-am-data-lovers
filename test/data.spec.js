dea15f2f71802efa392b31d1ff380f20d4895c0a
carocampos <ccampossalazar@gmail.com>
2018-12-19 08:35:36 -0500

    Añadiendo test de filtrado

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,29 @@
 require('../src/data.js');
 
+const input = 
+[
+  {id: 1, "num": "001", "name": "Bulbasaur", "type": [
+    "Grass",
+    "Poison"]
+  },
+  {"id": 4, "num": "004", "name": "Charmander", "type": [
+      "Fire"]
+  },
+  {"id": 7, "num": "007", "name": "Squirtle", "type": [
+      "Water"]
+  },
+];
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('pokemon', () => {
+  it('Debería ser un objeto', () => {
+    expect(typeof pokemon).toBe('object');
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  describe('pokemon.filterData', () => {
+    it('Debería ser una función', () => {
+      expect(typeof window.pokemon.filterData).toBe('function');
+    });
+    it('Debería retornar un array de objetos, con la propiedad type' , () => {
+      expect();
+    });
   });
 });
