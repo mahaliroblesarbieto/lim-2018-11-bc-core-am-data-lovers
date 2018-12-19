211f67cb638ca54f6b594d96c3b89d02b2e1f77a
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2018-12-18 22:26:50 -0500

    eslint sin errores

--- src/data.js
+++ src/data.js
@@ -1,41 +1,35 @@
-window.POKEMON  = POKEMON;
-const pokemonLength  = window.POKEMON.pokemon.length;
-function filterData(data,condition){
-  switch (data) {
+
+window.pokemon = {
+  filterData: function(data, condition) {
+    let listIdPokemonType = [];
+    switch (data) {
     case 'type':
-      return filterTypeOfPokemon(condition);
+      listIdPokemonType = window.POKEMON.pokemon.filter(compare => (compare.type[0] === condition || compare.type[1] === condition || compare.type[2] === condition));
       break;
     case 'egg':
-      return filterEggOfPokemon(condition);
+      listIdPokemonType = window.POKEMON.pokemon.filter(compare => (compare.egg === condition));
       break;
     default:
-  }
-}
-function filterTypeOfPokemon(typePokemonValue){
-  let listIdPokemonType = [];
-  listIdPokemonType = window.POKEMON.pokemon.filter(compare  =>  (compare.type[0]  ==  typePokemonValue || compare.type[1]  ==  typePokemonValue || compare.type[2]  ==  typePokemonValue));
-  return  listIdPokemonType;
-}
-function filterEggOfPokemon(eggPokemonValue){
-  const arrFilter = window.POKEMON.pokemon.filter(compare  =>  (compare.egg  ==  eggPokemonValue));
-  return arrFilter;
-}
-function sortData(data,sortBy,condition){
-  let listPokemonOrder = [];
-  switch (condition) {
-    case 'asc':
-    if(sortBy == 'number'){
-      listPokemonOrder = data.sort((a, b) => (a.id > b.id ? 1 : -1));
-      return listPokemonOrder;
-    }else{
-      listPokemonOrder = data.sort((a, b) => (a.name > b.name ? 1 : -1));
-      return listPokemonOrder;
     }
+    return listIdPokemonType;
+  },
+  sortData: function(data, sortBy, condition) {
+    let listPokemonOrder = [];
+    switch (condition) {
+    case 'asc':
+      if (sortBy === 'number') {
+        listPokemonOrder = data.sort((aa, bb) => (aa.id > bb.id ? 1 : -1));
+      } else {
+        listPokemonOrder = data.sort((aa, bb) => (aa.name > bb.name ? 1 : -1));
+      }
       break;
     case 'desc':
-    listPokemonOrder = data.sort((a, b) => (a.name > b.name ? 1 : -1));
-    return listPokemonOrder.reverse();
+      listPokemonOrder = data.sort((aa, bb) => (aa.name > bb.name ? 1 : -1)).reverse();
       break;
     default:
+    }
+    return listPokemonOrder;
   }
-}
+};
+
+
