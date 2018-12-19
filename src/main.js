10877254646436eb1eeceeb7b82d4d8360ee32d1
carocampos <ccampossalazar@gmail.com>
2018-12-18 22:47:50 -0500

    Se modificó data.js y main.js para test y carpeta package-lock.json

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+const pokemonData = window.POKEMON.pokemon;
+
+const pokemon = window.pokemon;
 let showPokemonFilter = '';
-for (let i = 0;  i < 151;  i ++) {
-  let imagen = window.POKEMON.pokemon[i].img;
-  let name = window.POKEMON.pokemon[i].name;
- showPokemonFilter += '<article class="col-md-3 col-sm-4 col-xs-6">' +
- '<div class="pokemon col-md-12 col-sm-12 col-xs-12">' +
- '<img src="'+imagen+'" border="1" >'+'<h3>'+name+'</h3>' +
- '</div>' + '</article>';
+for (let i = 0; i < 151; i++) {
+  let imagen = pokemonData[i].img;
+  let name = pokemonData[i].name;
+  showPokemonFilter += '<article class="col-md-3 col-sm-4 col-xs-6">' +
+    '<div class="pokemon col-md-12 col-sm-12 col-xs-12">' +
+    '<img src="' + imagen + '<h3>' + name + '</h3>' +
+    '</div>' + '</article>';
 }
 document.getElementById('pokemon-list').innerHTML = showPokemonFilter;
 
@@ -13,57 +16,57 @@ const typePokemon = document.getElementById('pokemon-type');
 typePokemon.addEventListener('change', () => {
   const typeData = 'type';
   const typePokemonValue = typePokemon.value;
-  const arrListOfIdPokemonType = filterData(typeData,typePokemonValue);
+  const arrListOfIdPokemonType = pokemon.filterData(typeData, typePokemonValue);
   let showPokemonFilter = '';
-  for (let i = 0;  i < arrListOfIdPokemonType.length;  i++) {
+  for (let i = 0; i < arrListOfIdPokemonType.length; i++) {
     showPokemonFilter += '<article class="col-md-3 col-sm-4 col-xs-6">' +
-   '<div class="pokemon col-md-12 col-sm-12 col-xs-12">'+
-   '<img src="'+arrListOfIdPokemonType[i].img+'" border="1">'+'<h3>'+arrListOfIdPokemonType[i].name+'</h3>' +
-   '</div>'+ '</article>';
+      '<div class="pokemon col-md-12 col-sm-12 col-xs-12">' +
+      '<img src="' + arrListOfIdPokemonType[i].img + '<h3>' + arrListOfIdPokemonType[i].name + '</h3>' +
+      '</div>' + '</article>';
   }
   document.getElementById('pokemon-list').innerHTML = showPokemonFilter;
 });
 
-const eggPokemon  = document.getElementById('pokemon-egg');
-eggPokemon.addEventListener('change', ()  =>  {
+const eggPokemon = document.getElementById('pokemon-egg');
+eggPokemon.addEventListener('change', () => {
   const typeData = 'egg';
   const eggPokemonValue = eggPokemon.value;
-  const arrPokemonEgg = filterData(typeData,eggPokemonValue);
-  let showPokemonFilter = "";
-  for (let i = 0;  i < arrPokemonEgg.length;  i++) {
+  const arrPokemonEgg = pokemon.filterData(typeData, eggPokemonValue);
+  let showPokemonFilter = '';
+  for (let i = 0; i < arrPokemonEgg.length; i++) {
     showPokemonFilter += '<article class="col-md-3 col-sm-4 col-xs-6">' +
-   '<div class="pokemon col-md-12 col-sm-12 col-xs-12">'+
-   '<img src="'+ arrPokemonEgg[i].img+'" border="1">'+'<h3>'+arrPokemonEgg[i].name+'</h3>' +
-   '</div>'+ '</article>';
+      '<div class="pokemon col-md-12 col-sm-12 col-xs-12">' +
+      '<img src="' + arrPokemonEgg[i].img + '<h3>' + arrPokemonEgg[i].name + '</h3>' +
+      '</div>' + '</article>';
   }
   document.getElementById('pokemon-list').innerHTML = showPokemonFilter;
 });
 
 const orderPokemon = document.getElementById('pokemon-order');
-orderPokemon.addEventListener('change', () =>  {
-  const arrPokemon = window.POKEMON.pokemon;
+orderPokemon.addEventListener('change', () => {
+  const arrPokemon = pokemonData;
   const orderPokemonValue = orderPokemon.value;
   let order;
   switch (orderPokemonValue) {
-    case 'number':
+  case 'number':
     order = 'asc';
-      break;
-    case 'name-one':
+    break;
+  case 'name-one':
     order = 'asc';
-      break;
-    case 'name-two':
+    break;
+  case 'name-two':
     order = 'desc';
-      break;
-    default:
+    break;
+  default:
   }
 
-  const orderPokemonName = sortData(arrPokemon,orderPokemonValue,order);
+  const orderPokemonName = pokemon.sortData(arrPokemon, orderPokemonValue, order);
   let showPokemonOrder = '';
-  for(let i = 0;  i < orderPokemonName.length;  i++){
-   showPokemonOrder += '<article class="col-md-3 col-sm-4 col-xs-6">' +
-   '<div class="pokemon col-md-12 col-sm-12 col-xs-12">'+
-   '<img src="'+orderPokemonName[i].img+'" border="1">'+'<h3>'+orderPokemonName[i].name+'</h3>' +
-   '</div>'+ '</article>';
+  for (let i = 0; i < orderPokemonName.length; i++) {
+    showPokemonOrder += '<article class="col-md-3 col-sm-4 col-xs-6">' +
+      '<div class="pokemon col-md-12 col-sm-12 col-xs-12">' +
+      '<img src="' + orderPokemonName[i].img + '<h3>' + orderPokemonName[i].name + '</h3>' +
+      '</div>' + '</article>';
   }
   document.getElementById('pokemon-list').innerHTML = showPokemonOrder;
 });
